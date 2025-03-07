6f55a0eb6f4a5fb3c56adc59d46337404fa4f627
Cheneba <chenebametuge14@gmail.com>
2025-03-07 06:29:11 +0100

    created the CRUD operations for the user, with the connection to the database passed as parameter to each method

---
+++ classes/user.php
@@ -0,0 +1,68 @@
+<?php
+class User
+{
+
+  // protected $conn = $connection;
+
+  public function getAll($conn)
+  {
+    $sql = "SELECT * FROM users";
+
+    $result = mysqli_query($conn, $sql);
+
+    $rows = mysqli_fetch_all($result);
+    if ($rows[0]) {
+      return $rows;
+    } else {
+      return false;
+    }
+  }
+  public function create($name, $password, $conn)
+  {
+    $sql = "SELECT * FROM `users` WHERE name = '" . $name . "' AND password = '" . $password . "'";
+    $result = mysqli_query($conn, $sql);
+    $rows = mysqli_fetch_all($result);;
+    if ($rows[0]) {
+      return $rows[0];
+    } else {
+      return false;
+    }
+  }
+  public function getOne($id, $conn)
+  {
+    $sql = "SELECT * FROM users WHERE id=$id";
+
+    $result = mysqli_query($conn, $sql);
+
+    $rows = mysqli_fetch_all($result);
+    if ($rows[0]) {
+      return $rows;
+    } else {
+      return false;
+    }
+  }
+
+  public function update($id, $param, $value, $conn)
+  {
+    $field_name = $param == 'p' ? "password" : "name";
+    $sql = "UPDATE users SET " . $field_name . " = $value WHERE users.id = $id";
+
+    if (mysqli_query($conn, $sql)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function delete($id, $conn)
+  {
+    $sql = "DELETE FROM users WHERE users.id = $id";
+
+    if (mysqli_query($conn, $sql)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public function deleteAll($conn) {}
+}
