92c3a84aca49a407f4901cef88dcf3056e4f58cd
Cheneba <chenebametuge14@gmail.com>
2025-03-12 05:13:38 +0100

    Implemented the getPost() function

--- includes/functions.php
+++ includes/functions.php
@@ -4,19 +4,29 @@ include "../config/database.php";
 include "../classes/post.php";
 include "../classes/user.php";
 
-$user = new User();
-$post = new Post();
+$user_object = new User();
+$post_object = new Post();
 
-var_dump($user->getAll($connection));
-echo "<br>";
-echo "<br>";
-var_dump($post->getAll($connection));
+// var_dump($user_object->getAll($connection));
+// echo "<br>";
+// echo "<br>";
+// var_dump($post_object->getAll($connection));
 
 function login() {}
 function logout() {}
 function getAllPosts() {}
 function getPostsWithUserId() {}
-function getPost() {}
+function getPost($id)
+{
+  global $post_object;
+  global $user_object;
+  global $connection;
+
+  $author_id = $post_object->getOne($id, $connection)[0][5] ?: false;
+  $post = $post_object->getOne($id, $connection)[0] ?: false;
+  $author = $user_object->getOne($author_id, $connection)[0][1] ?: false;
+  return ["post" => $post, "author" => $author];
+}
 function editPostWithUserId() {}
 function newPostWithUserId() {}
 function deletePostWithUserId() {}
